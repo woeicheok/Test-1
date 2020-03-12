cfe3a0392d3a670bc140da57c8875ce14e29da50
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-12 15:30:57 +0800

    Add files via upload

---
+++ testQ3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class testQ3
+{
+     public static void main(String[]args)
+     {
+     
+     Scanner in = new Scanner(System.in);
+     System.out.println("Please Enter a Sentences: ");
+     String sentences1 = in.next();
+     System.out.println("Please Enter another sentences: ");
+     String sentences2 = in.next();
+     
+     if(sentences1.equals(sentences2)){
+      System.out.println("Sentences1 and sentences2 are equal");
+      }
+      else{
+      System.out.println("Sentences1 is not equal with sentences2");
+      }
+      
+      }
+}
+      
