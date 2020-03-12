46a0429ac84d5d118c7c8b8cc77e6172de072ee9
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-12 15:30:32 +0800

    Add files via upload

---
+++ testQ2.java
@@ -0,0 +1,24 @@
+public class testQ2
+{
+    static int i;
+    static byte b;
+    static short s;
+    static long l;
+    static float f;
+    static double d;
+    static char c;
+    static boolean B;
+    
+    public static void main(String[]args)
+    {
+    
+        System.out.println("Int : " +i);
+        System.out.println("Byte : " + b);
+        System.out.println("Short : " + s);
+        System.out.println("Long : " + l);
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        System.out.println("Char : " + c);
+        System.out.println("Boolean : " + B);
+    }
+}
