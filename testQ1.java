57595d3fe3788f13d4c71c4fca414aade860ff52
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-12 15:30:11 +0800

    Add files via upload

---
+++ testQ1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class testQ1
+{
+   public static void main(String[]args)
+   {
+   Scanner in = new Scanner(System.in);
+   
+   System.out.println("Enter any message");
+   String message = in.next();
+   
+   System.out.println("Your message: " + message);
+   }
+}   
